9d7c9a6a2b5c8d5a8cd8f447e97eaf9d38bc45c2
sakhb <druzhininsergey96@gmail.com>
2023-11-10 20:31:27 +1100

    KS-1-3 edited spaces

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,9 +1,9 @@
 package lesson_1
 
 fun main() {
-    val numberOfOrders :Int = 75
-    val goodbyeMessage :String = "Спасибо Вам за покупку, приходите еще"
-    var numberOfEmployees : Int = 2000
+    val numberOfOrders: Int = 75
+    val goodbyeMessage: String = "Спасибо Вам за покупку, приходите еще"
+    var numberOfEmployees: Int = 2000
 
     println(numberOfOrders)
     println(goodbyeMessage)
