6be63b4e04c79fe82a37fed00a857a2398ac95d4
sakhb <druzhininsergey96@gmail.com>
2023-11-10 18:43:48 +1100

    KS-1-2 employees

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders :Int = 75
+    val goodbyeMessage :String = "Спасибо Вам за покупку, приходите еще"
+    var numberOfEmployees : Int = 2000
+
+    println(numberOfOrders)
+    println(goodbyeMessage)
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
