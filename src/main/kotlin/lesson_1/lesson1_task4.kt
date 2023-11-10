917889a7accbda721f1234b2aaabd7f686c4948b
sakhb <druzhininsergey96@gmail.com>
2023-11-10 21:10:22 +1100

    KS-1-4 Removed unnecessary space

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -7,7 +7,7 @@ fun main () {
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: UInt  = 327000u
+    val apogee: UInt = 327000u
 
     println(length)
     println(age)
