e23edd8c563c2c7aab7619d747cd3ba5c3e4920a
sakhb <druzhininsergey96@gmail.com>
2023-11-10 17:45:11 +1100

    KS-1-1 Word error corrected

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package lesson_1
 
 fun main() {
     val numberOfOrders :Int = 75
-    val goodbyeMassage :String = "Спасибо Вам за покупку, приходите еще"
+    val goodbyeMessage :String = "Спасибо Вам за покупку, приходите еще"
 }
