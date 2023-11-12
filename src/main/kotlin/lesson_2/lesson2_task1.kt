65aa1a82f46276246151276b9da6facaae07d73a
sakhb <druzhininsergey96@gmail.com>
2023-11-13 00:35:50 +1100

    KS-2-1 mean

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+
+    val student1 = 3.00
+    val student2 = 4.00
+    val student3 = 3.00
+    val student4 = 5.00
+
+    val mean = (student1 + student2 + student3 + student4) / 4
+
+    println(mean)
+}
