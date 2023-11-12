f48d8437d53d7f8f44f3b5a9e4d0c630eef32a75
sakhb <druzhininsergey96@gmail.com>
2023-11-13 03:26:40 +1100

    KS-3-1 greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+
+    var lastName = "Андреева"
+    val name = "Татьяна"
+    val patronymicName = "Сергеевна"
+    var age = 20
+
+    println("$lastName $name $patronymicName, $age лет")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $name $patronymicName, $age года")
+}
