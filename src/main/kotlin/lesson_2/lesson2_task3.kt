7679471723c992eadcb47348911da11141d65393
sakhb <druzhininsergey96@gmail.com>
2023-11-14 21:27:58 +1100

    KS-2-3 renamed const

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,21 +1,21 @@
 package lesson_2
 
-const val minutesInHour = 60
+const val MINUTES_IN_HOUR = 60
 fun main () {
 
     val departureHours = 9
     val departureMinutes = 39
     val travelTime: Int = 457
 
-    val travelTimeHours = travelTime / minutesInHour
-    val travelTimeMinutes = travelTime % minutesInHour
+    val travelTimeHours = travelTime / MINUTES_IN_HOUR
+    val travelTimeMinutes = travelTime % MINUTES_IN_HOUR
 
     var arrivalHours = departureHours + travelTimeHours
     var arrivalMinutes = departureMinutes + travelTimeMinutes
 
-    if (arrivalMinutes >= minutesInHour) {
+    if (arrivalMinutes >= MINUTES_IN_HOUR) {
         arrivalHours++
-        arrivalMinutes -= minutesInHour
+        arrivalMinutes -= MINUTES_IN_HOUR
     }
     else arrivalMinutes
 
