48e6f76fbdccf4205b09bd9a86120b0a383066fc
sakhb <druzhininsergey96@gmail.com>
2023-11-17 03:47:45 +1100

    KS-4-3 corrections according to the remarks

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,18 +1,18 @@
 package lesson_4
 
+const val isSunny = true
+const val isAwningOpen = true
+const val humidity = 20
+const val seasonForBeans = "Зима"
+
 fun main() {
 
-    val sun = true
-    val tartOpened = true
-    val humidity = 20
-    val season = "Не зима"
-
-    val sunToday = true
-    val tartOpenedToday = true
+    val isSunnyToday = true
+    val isAwningOpenToday = true
     val humidityToday = 20
     val seasonToday = "Зима"
 
-    val conditionsForBeans = (sun == sunToday) && (tartOpened == tartOpenedToday) && (humidity == humidityToday) && (season == seasonToday)
+    val conditionsForBeans = (isSunny == isSunnyToday) && (isAwningOpen == isAwningOpenToday) && (humidity == humidityToday) && (seasonForBeans != seasonToday)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsForBeans")
 
