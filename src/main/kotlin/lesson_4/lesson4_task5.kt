d89370e804f67f36893f2255e55b8481668674b4
sakhb <druzhininsergey96@gmail.com>
2023-11-17 05:23:48 +1100

    KS-4-5 removed unnecessary duplications

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,33 +1,33 @@
 package lesson_4
 
+const val IS_SHIP_DAMAGED = false
+const val CREW_MIN = 55
+const val CREW_MAX = 70
+const val PROVISION_BOXES = 50
+const val IS_WEATHER_GOOD = true
+
 fun main() {
 
-    val shipIsNotDamaged = true
-    val crew = 55..70
-    val provisionBoxes = 50
-    val weatherIsGood = true
-
-
-    println("Проверка состояния корабля. Введите true если корабль не имеет повреждений и false если имеются поврежнения")
+    println("""Проверка состояния корабля. Введите: 
+        |true - если у корабля имеются повреждения 
+        |false - если повреждения отсутствуют""".trimMargin())
     val shipDamaged = readln().toBoolean()
     println(shipDamaged)
 
-    if (shipDamaged == shipIsNotDamaged) {
-        println("Введите текущий состав экипажа:")
-        val crewCheck = readln().toIntOrNull()
-        println("Введите количество ящиков с провизией на борту:")
-        val provisionCheck = readln().toIntOrNull()
-        val conditionForVoyage = (crewCheck in crew) && (provisionCheck!! >= provisionBoxes)
+    println("Введите текущий состав экипажа:")
+    val crewCheck = readln().toIntOrNull()
+    println("Введите количество ящиков с провизией на борту:")
+    val provisionCheck = readln().toIntOrNull()
+
+    if (shipDamaged == IS_SHIP_DAMAGED) {
+        val conditionForVoyage = (crewCheck in CREW_MIN..CREW_MAX) && (provisionCheck!! >= PROVISION_BOXES)
         println("Готовность корабля к путешествию: $conditionForVoyage")
     }
-    else {println("Введите текущий состав экипажа:")
-        val crewCheck = readln().toIntOrNull()
-        println("Введите количество ящиков с провизией на борту:")
-        val provisionCheck = readln().toIntOrNull()
+    else {
         println("Метеоусловия благоприятные? Введите true если благоприятные и false если неблагоприятные")
         val weatherCheck = readln().toBoolean()
         println(weatherCheck)
-        val conditionForVoyage = (crewCheck in crew) && (provisionCheck!! >= provisionBoxes) && (weatherCheck == weatherIsGood)
+        val conditionForVoyage = (crewCheck in CREW_MIN..CREW_MAX) && (provisionCheck!! >= PROVISION_BOXES) && (weatherCheck == IS_WEATHER_GOOD)
         println("Готовность корабля к путешествию: $conditionForVoyage")
     }
 }
