5e5735c9fb1bddbefd84fd34d306756f0789e51d
sakhb <druzhininsergey96@gmail.com>
2023-11-16 17:25:54 +1100

    KS-3-4 reduced

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -2,20 +2,16 @@ package lesson_3
 
 fun main() {
 
-    var fromLetter = "E"
-    var fromNumber = 2
-    var toLetter = "E"
-    val toNumber = 4
+    var from = "E2"
+    var to = "E4"
     var moveNumber = 1
 
-    var fullMoveDescription = "$fromLetter$fromNumber-$toLetter$toNumber;$moveNumber"
+    var fullMoveDescription = "$from-$to;$moveNumber"
     println(fullMoveDescription)
 
-    fromLetter = "D"
-    fromNumber = 2
-    toLetter = "D"
-    moveNumber ++
+    from = "D2"
+    to = "D3"
 
-    fullMoveDescription = "$fromLetter$fromNumber-$toLetter${fromNumber + 1};$moveNumber"
+    fullMoveDescription = "$from-$to;${++moveNumber}"
     println(fullMoveDescription)
 }
