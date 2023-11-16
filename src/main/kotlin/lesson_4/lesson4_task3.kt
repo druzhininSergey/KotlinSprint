75cc1ded38d6ed3a2e1734122a7c301ca287fcac
sakhb <druzhininsergey96@gmail.com>
2023-11-17 06:43:29 +1100

    KS-4-3 changed const names

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,18 +1,19 @@
 package lesson_4
 
-const val isSunny = true
-const val isAwningOpen = true
-const val humidity = 20
-const val seasonForBeans = "Зима"
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
+const val HUMIDITY = 20
+const val SEASON_FOR_BEANS = "Зима"
 
 fun main() {
 
     val isSunnyToday = true
     val isAwningOpenToday = true
+
     val humidityToday = 20
     val seasonToday = "Зима"
 
-    val conditionsForBeans = (isSunny == isSunnyToday) && (isAwningOpen == isAwningOpenToday) && (humidity == humidityToday) && (seasonForBeans != seasonToday)
+    val conditionsForBeans = (IS_SUNNY == isSunnyToday) && (IS_AWNING_OPEN == isAwningOpenToday) && (HUMIDITY == humidityToday) && (SEASON_FOR_BEANS != seasonToday)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsForBeans")
 
