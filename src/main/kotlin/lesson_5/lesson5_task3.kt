b717669419d912620dcf4799d4b2b63893ea5b4b
sakhb <druzhininsergey96@gmail.com>
2023-11-19 06:40:40 +1100

    KS-5-3 reverse check

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -3,7 +3,7 @@ package lesson_5
 const val DOUGLAS_ADAMS_NUMBER = 42
 const val FIBONACCI_SEQUENCE_NUMBER = 21
 const val NUMBERS_IN_GAME = "Здравствуйте, напоминаем Вам, что в игре участвуют числа от 0 до 42."
-const val LOST_MESSAGE = "Правильные числа были 42 и 21 соответственно"
+const val LOST_MESSAGE = "Правильные числа были 42 и 21"
 
 fun main() {
 
@@ -13,8 +13,14 @@ fun main() {
     val userFibonacciNumber = readln().toIntOrNull()
 
     when {
-        userFibonacciNumber == FIBONACCI_SEQUENCE_NUMBER && userDouglasNumber == DOUGLAS_ADAMS_NUMBER -> println("Поздравляем, вы выиграли главный приз")
-        userFibonacciNumber == FIBONACCI_SEQUENCE_NUMBER || userDouglasNumber == DOUGLAS_ADAMS_NUMBER -> println("Вы выиграли утешительный приз. $LOST_MESSAGE")
+        (userDouglasNumber == DOUGLAS_ADAMS_NUMBER && userFibonacciNumber == FIBONACCI_SEQUENCE_NUMBER)
+                || (userDouglasNumber == FIBONACCI_SEQUENCE_NUMBER && userFibonacciNumber == DOUGLAS_ADAMS_NUMBER)
+        -> println("Поздравляем, вы выиграли главный приз")
+
+        (userDouglasNumber == DOUGLAS_ADAMS_NUMBER || userFibonacciNumber == FIBONACCI_SEQUENCE_NUMBER)
+                || (userDouglasNumber == FIBONACCI_SEQUENCE_NUMBER || userFibonacciNumber == DOUGLAS_ADAMS_NUMBER)
+        -> println("Вы выиграли утешительный приз. $LOST_MESSAGE")
+
         else -> println("Неудача. $LOST_MESSAGE")
     }
 }
