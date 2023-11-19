b364c40ec15f19cb2ae2f4bca45bb98f6cd84745
sakhb <druzhininsergey96@gmail.com>
2023-11-20 02:52:37 +1100

    KS-6-5 human test

---
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -0,0 +1,28 @@
+package lesson_6
+
+fun main() {
+
+    val numbersForTest = 1..9
+    var attemptsAmount = 3
+    println("Пожалуйста, пройдите проверку, чтобы мы убедились, что вы не человек.")
+
+    do {
+        val firstRandomNumber = numbersForTest.random()
+        val secondRandomNumber = numbersForTest.random()
+        val sumAnswerForTest = firstRandomNumber + secondRandomNumber
+        println("Чему равна сумма чисел $firstRandomNumber и $secondRandomNumber")
+        println("Введите результат:")
+        val userTestAnswer = readln().toIntOrNull()
+
+        if (userTestAnswer != null && userTestAnswer != userTestAnswer) {
+            attemptsAmount--
+            println("Результат сложения неверный, попробуйте снова. Осталось попыток: $attemptsAmount")
+            if (attemptsAmount == 0) println("Доступ запрещен")
+        } else if (userTestAnswer != null && userTestAnswer == sumAnswerForTest) println("Добро пожаловать!")
+        else {
+            attemptsAmount--
+            println("Введен некорректный символ, осталось попыток: $attemptsAmount")
+            if (attemptsAmount == 0) println("Доступ запрещен")
+        }
+    } while (attemptsAmount > 0 && userTestAnswer != sumAnswerForTest)
+}
