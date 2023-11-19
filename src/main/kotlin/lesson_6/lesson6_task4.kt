28de3e43a21ee7b167a640ac5c0450578ca75676
sakhb <druzhininsergey96@gmail.com>
2023-11-19 23:46:20 +1100

    KS-6-4 lottery_2.0

---
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -0,0 +1,29 @@
+package lesson_6
+
+const val GREETING_AND_INSTRUCTIONS = "Привет! Чтобы выиграть эту игру, нужно угадать число от 1 до 9." +
+        " У тебя 5 попыток, удачи!"
+
+fun main() {
+
+    val numberInGame = 1..9
+    val randomNumber = numberInGame.random()
+    var numbersOfAttempts = 5
+    val failedAttemptMessages = listOf("Упс, мимо", "Неа", "Не угадал ха-ха", "Близко, но нет", "Не вышло...")
+    println(GREETING_AND_INSTRUCTIONS)
+
+    do {
+        println("Введи число:")
+        val userNumberChoice = readln().toIntOrNull()
+        if (userNumberChoice != null && userNumberChoice != randomNumber) {
+            println(failedAttemptMessages.random())
+            println("Осталось попыток: ${--numbersOfAttempts}")
+            if (numbersOfAttempts == 0) println("GAME OVER нужно было написать всего лишь число $randomNumber эх ты...")
+        } else if (userNumberChoice != null && userNumberChoice == randomNumber)
+            println("Это была великолепная игра!!!")
+        else {
+            println("Что-то не то ты ввел, но попытка засчитана, молодец, попробуй еще...")
+            println("У тебя осталось ${--numbersOfAttempts} попыток")
+            if (numbersOfAttempts == 0) println("GAME OVER нужно было написать всего лишь число $randomNumber эх ты...")
+        }
+    } while (numbersOfAttempts > 0 && userNumberChoice != randomNumber)
+}
