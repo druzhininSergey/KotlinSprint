e97ca3aa9f1cc0bf23bbc3abd4055627a9912a47
sakhb <druzhininsergey96@gmail.com>
2023-11-19 23:44:21 +1100

    KS-6-3 timer_2.0

---
+++ src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -0,0 +1,16 @@
+package lesson_6
+
+fun main() {
+
+    println("Для запуска таймера, введите время в секундах, чтобы начать отчет")
+    var userTimerSeconds = readln().toIntOrNull()
+
+    if (userTimerSeconds != null) {
+        while (userTimerSeconds > 0) {
+            println("Осталось секунд: ${userTimerSeconds--}")
+            Thread.sleep(1000)
+        }
+        println("Время вышло")
+    }
+    else println("Введен некорректный символ, попробуйте снова")
+}
