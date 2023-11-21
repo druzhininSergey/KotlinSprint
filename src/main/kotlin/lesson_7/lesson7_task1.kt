b53030049a4afcf87a0a95d708a051aeedb1946a
sakhb <druzhininsergey96@gmail.com>
2023-11-21 22:16:38 +1100

    KS-7-1 declared variable before the loop

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -4,9 +4,10 @@ fun main() {
 
     val letterForPassword = 'a'..'z'
     val digitForPassword = 0..9
+    var randomPassword = ""
 
     for (i in 1..3) {
-        val randomPassword = "${letterForPassword.random()}${digitForPassword.random()}"
-        print(randomPassword)
+        randomPassword += "${letterForPassword.random()}${digitForPassword.random()}"
     }
+    println(randomPassword)
 }
