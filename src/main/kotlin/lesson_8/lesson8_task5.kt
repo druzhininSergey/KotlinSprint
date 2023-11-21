5ce5eef1670f9796a01b9a887978a9684f1d3c6f
sakhb <druzhininsergey96@gmail.com>
2023-11-22 07:57:13 +1100

    KS-8-5 new array from user

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,18 @@
+package lesson_8
+
+fun main() {
+
+    var arrayOfIngredients = emptyArray<String>()
+    println("Укажите какое количество ингредиентов вы хотите добавить в список:")
+    val userAmountOfIngredients = readln().toIntOrNull() ?: 0
+
+    for (i in 0 until userAmountOfIngredients) {
+        println("Введите ${i + 1}-й ингредиент")
+        val ingredientToAdd = readln()
+        arrayOfIngredients += ingredientToAdd
+    }
+    println("Ваш новый список ингредиентов:")
+    for (i in 0..arrayOfIngredients.lastIndex) {
+        println("Ингредиент № ${i + 1}: ${arrayOfIngredients[i]}")
+    }
+}
