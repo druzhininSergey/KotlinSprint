dfcef80c58ccbbe59b2a1043eb2fd161ef4901e4
sakhb <druzhininsergey96@gmail.com>
2023-11-24 05:13:32 +1100

    KS-8-5 arrayOfNulls

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -2,17 +2,16 @@ package lesson_8
 
 fun main() {
 
-    var arrayOfIngredients = emptyArray<String>()
     println("Укажите какое количество ингредиентов вы хотите добавить в список:")
     val userAmountOfIngredients = readln().toIntOrNull() ?: 0
+    val arrayOfIngredients = arrayOfNulls<String>(userAmountOfIngredients)
 
-    for (i in 0 until userAmountOfIngredients) {
+    for (i in 0..<userAmountOfIngredients) {
         println("Введите ${i + 1}-й ингредиент")
-        val ingredientToAdd = readln()
-        arrayOfIngredients += ingredientToAdd
+        arrayOfIngredients[i] = readln()
     }
     println("Ваш новый список ингредиентов:")
     for (i in arrayOfIngredients) {
-        println("Ингредиент № ${arrayOfIngredients.indexOf(i)+1}: $i")
+        println("Ингредиент № ${arrayOfIngredients.indexOf(i) + 1}: $i")
     }
 }
