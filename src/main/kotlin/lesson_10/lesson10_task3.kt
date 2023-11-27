453f2348350404885a1c36e6f26abb28e73fdf22
sakhb <druzhininsergey96@gmail.com>
2023-11-28 09:49:49 +1100

    KS-10-3 password any length

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,21 @@
+package lesson_10
+
+fun main() {
+
+    println("Введите количество символов для создания пароля: ")
+    val passwordLength = readln().toIntOrNull() ?: 0
+    inputLength(passwordLength)
+}
+
+fun inputLength(length: Int) {
+    val digits = "0123456789"
+    val symbols = "!\"#\$%&'()*+,-./ "
+    var pass = ""
+
+    for (i in 0..<length) {
+        val digit = i % 2
+        pass += if (digit == 0) digits.random()
+        else symbols.random()
+    }
+    println(pass)
+}
