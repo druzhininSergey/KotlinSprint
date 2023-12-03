5c422c227b5231f0039a7491332902707a622a33
sakhb <druzhininsergey96@gmail.com>
2023-12-03 22:40:07 +1100

    KS-10-5 changed functions content

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -2,28 +2,30 @@ package lesson_10
 
 const val USER_LOGIN = "human"
 const val USER_PASSWORD = "12345"
-val humanCart = listOf("Сумка", "Рюкзак", "Ботинки")
 
 fun main() {
+    val humanCart = listOf("Сумка", "Рюкзак", "Ботинки")
+    val token = authorizeUser()
+
+    if (token == "") println("Такой логин или пароль не существует")
+    else humanCart.forEach { println(it) }
+}
+
+fun authorizeUser(): String {
     println("Введите логин:")
     val loginToCheck = readln()
     println("Введите пароль:")
     val passwordToCheck = readln()
-
-    val token = authorizeUser(loginToCheck, passwordToCheck)
-    if (token != null) getToken()
-    else println("Такой логин или пароль не существует")
+    return getToken(loginToCheck, passwordToCheck)
 }
 
-fun authorizeUser(loginToCheck: String, passwordToCheck: String): String? {
-    return if (loginToCheck == USER_LOGIN && passwordToCheck == USER_PASSWORD) ({
+fun getToken(loginToCheck: String, passwordToCheck: String): String {
+    var token = ""
+    if (loginToCheck == USER_LOGIN && passwordToCheck == USER_PASSWORD) {
         val symbolsForToken = (1..9) + ('a'..'z') + ('A'..'Z')
-        var token = ""
-        for (i in 1..32) token += symbolsForToken.random()
-    }).toString()
-    else null
+        for (i in 1..32) {
+            token += symbolsForToken.random()
+        }
+    }
+    return token
 }
-
-fun getToken(): Unit = humanCart.forEach { println(it) }
-
-
