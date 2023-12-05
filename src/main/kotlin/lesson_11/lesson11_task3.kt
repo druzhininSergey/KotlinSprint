28463f2403d14a180f4c58afcb2f3d90e8cb3eb8
sakhb <druzhininsergey96@gmail.com>
2023-12-06 02:11:31 +1100

    KS-11-3 deleted User class

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,69 +1,63 @@
 package lesson_11
 
-//class Member(
-//    private val membersInRoomAvatars: MutableList<User>,
-//) {
-//    fun addUserToRoom(user: User) {
-//        membersInRoomAvatars.add(user)
-//    }
-//
-//    fun showUserAvatars() {
-//        for (user in membersInRoomAvatars) {
-//            println(user.userAvatar)
-//        }
-//    }
-//
-//    fun showUserInfo(avatar: String) {
-//        val userInfo = membersInRoomAvatars.find { it.userAvatar == avatar }
-//        if (userInfo != null) {
-//            println(userInfo.userName)
-//            println(userInfo.userStatus)
-//        } else println("Этот пользователь скрыл информацию о себе")
-//    }
-//}
-//
-//class User(
-//    val userName: String,
-//    val userAvatar: String,
-//    val userStatus: String,
-//)
-//
-//class Room(
-//    private val frontCover: String,
-//    private val roomName: String,
-//) {
-//    fun printRoomInfo() {
-//        println(frontCover)
-//        println(roomName)
-//    }
-//}
-//
-//fun main() {
-//
-//    val room1 = Room(
-//        "IMG",
-//        "Игры",
-//    )
-//    val user1 = User(
-//        "Faker",
-//        "Picture1",
-//        "Разговаривает",
-//    )
-//    val user2 = User(
-//        "FALLEN",
-//        "Picture2",
-//        "Микрофон выключен"
-//    )
-//
-//    val member = Member(mutableListOf())
-//    member.addUserToRoom(user1)
-//    member.addUserToRoom(user2)
-//    room1.printRoomInfo()
-//    println("Пользователи в комнате: ")
-//    println()
-//    member.showUserAvatars()
-//
-//    println("Выберите аватарку, чей профиль вы хотите посмотреть")
-//    val chooseAvatar = readln()
-//    member.showUserInfo(chooseAvatar)
-//}
+class Member(
+    val userName: String,
+    val userAvatar: String,
+    val userStatus: String,
+)
+
+class Room(
+    private val frontCover: String,
+    private val roomName: String,
+    private var memberList: MutableList<Member> = mutableListOf(),
+) {
+    fun addUserToRoom(user: Member) {
+        memberList.add(user)
+    }
+
+    fun printRoomInfo() {
+        println(frontCover)
+        println(roomName)
+        println("Пользователи в комнате:")
+        println()
+        if (memberList.isNotEmpty()) {
+            for (i in memberList) {
+                println(i.userAvatar)
+            }
+        } else println("В этой комнате нет пользователей")
+    }
+
+    fun showUserInfo(avatar: String) {
+        val userInfo = memberList.find { it.userAvatar == avatar }
+        if (userInfo != null) {
+            println(userInfo.userName)
+            println(userInfo.userStatus)
+        } else println("Этот пользователь скрыл информацию о себе")
+    }
+}
+
+fun main() {
+
+    val room1 = Room(
+        "IMG",
+        "Игры",
+    )
+    val user1 = Member(
+        "Faker",
+        "Picture1",
+        "Разговаривает",
+    )
+    val user2 = Member(
+        "FALLEN",
+        "Picture2",
+        "Микрофон выключен"
+    )
+
+    room1.addUserToRoom(user1)
+    room1.addUserToRoom(user2)
+    room1.printRoomInfo()
+
+    println("Выберите аватарку, чей профиль вы хотите посмотреть")
+    val chooseAvatar = readln().trim().capitalize()
+    room1.showUserInfo(chooseAvatar)
+}
