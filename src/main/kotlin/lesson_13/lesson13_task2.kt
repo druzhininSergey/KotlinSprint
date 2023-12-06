fb2f1e9a0a0ff96bc7f1d539af570f49e337d4c0
sakhb <druzhininsergey96@gmail.com>
2023-12-06 19:04:26 +1100

    KS-13-2-print-phone-directory

---
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,28 @@
+package lesson_13
+
+class PhoneDirectory2(
+    val name: String,
+    val phoneNumber: Long,
+    val companyName: String? = null,
+) {
+    fun printContactData() {
+        if (companyName != null) {
+            println("-Имя: $name\n-Номер: $phoneNumber\n-Компания: $companyName")
+        } else println("-Имя: $name\n-Номер: $phoneNumber")
+    }
+}
+
+fun main() {
+    val contact1 = PhoneDirectory2(
+        "Сергей",
+        89997566644,
+    )
+    val contact2 = PhoneDirectory2(
+        "Михаил",
+        87549348645,
+        "Parker"
+    )
+    contact1.printContactData()
+    println()
+    contact2.printContactData()
+}
