1de8a680c60e7a562e83e2d88bb7e150c85f2f22
sakhb <druzhininsergey96@gmail.com>
2023-12-07 18:39:12 +1100

    KS-13-3 made the requested changes

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -5,8 +5,8 @@ class PhoneDirectory3(
     val phoneNumber: Long,
     val companyName: String? = null,
 ) {
-    fun printCompanyNames() {
-        if (companyName != null) println(companyName)
+    fun printCompanyName() {
+        println(companyName)
     }
 }
 
@@ -14,12 +14,12 @@ fun main() {
     val contactsList = mutableListOf(
         PhoneDirectory3("Дарт Вейдер", 89997566644, "Ситхи"),
         PhoneDirectory3("Оби-Ван Кеноби", 87549348645, "Орден Джедаев"),
-        PhoneDirectory3("Лея Органа", 87468376644, null),
-        PhoneDirectory3("Джабба Хатт", 84376478374, null),
+        PhoneDirectory3("Лея Органа", 87468376644),
+        PhoneDirectory3("Джабба Хатт", 84376478374),
         PhoneDirectory3("Генерал Гривус", 94337486734, "null"),
     )
     println("Компании телефонной книги:")
     for (i in contactsList) {
-        i.printCompanyNames()
+        if (i.companyName != null) i.printCompanyName()
     }
 }
