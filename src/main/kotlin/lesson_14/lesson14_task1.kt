9e2987f795299f39364acf1a67beb8ce0dfdf251
sakhb <druzhininsergey96@gmail.com>
2023-12-08 20:03:19 +1100

    KS-14-1 add ship names

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,27 +1,31 @@
 package lesson_14
 
 open class Ship(
+    val shipName: String,
     val speed: Int,
     val loadCapacity: Int,
     val hasIceBreakingAbility: Boolean = false,
 )
 
 class CargoShip(
+    shipName: String = "Грузовой корабль",
     speed: Int = 50,
     loadCapacity: Int = 500,
-) : Ship(speed, loadCapacity)
+) : Ship(shipName, speed, loadCapacity)
 
 class IceBreaker(
+    shipName: String = "Ледокол",
     speed: Int = 40,
     loadCapacity: Int = 150,
     hasIceBreakingAbility: Boolean = true,
-) : Ship(speed, loadCapacity, hasIceBreakingAbility)
+) : Ship(shipName, speed, loadCapacity, hasIceBreakingAbility)
 
 class Liner(
+    shipName: String = "Лайнер",
     speed: Int = 80,
     loadCapacity: Int = 200,
     val passengers: Int = 500,
-) : Ship(speed, loadCapacity)
+) : Ship(shipName, speed, loadCapacity)
 
 fun main() {
     val cargoShip = CargoShip()
