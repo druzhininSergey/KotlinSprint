55fe4822d78e8873eeeaa6614542db09547b7da6
sakhb <druzhininsergey96@gmail.com>
2023-12-12 09:11:40 +1100

    KS-14-4 deleted for loop

--- src/main/kotlin/lesson_14/lesson14_task4.kt
+++ src/main/kotlin/lesson_14/lesson14_task4.kt
@@ -13,15 +13,13 @@ class Planet(
     val satelliteList: MutableList<Satellite> = mutableListOf(),
 ) : CelestialBody(name, isSuitableForLanding, isAtmosphereBreathable) {
 
-    fun addSatellite(satellite: Satellite) = satelliteList.add(satellite)
+    fun addSatellite(list: List<Satellite>) = satelliteList.addAll(list)
 
     fun printPlanetAndSatelliteInfo() {
-        if (satelliteList.isNotEmpty()) {
-            println("Планета $name имеет спутники:")
-            for (i in satelliteList) {
-                println(i.name)
-            }
-        } else println("Планета $name не имеет спутников.")
+        println("Планета $name имеет спутники:")
+        for (i in satelliteList) {
+            println(i.name)
+        }
     }
 }
 
@@ -29,28 +27,14 @@ class Satellite(
     name: String,
     isAtmosphereBreathable: Boolean,
     isSuitableForLanding: Boolean,
-    var belongsToPlanet: String,
 ) : CelestialBody(name, isAtmosphereBreathable, isSuitableForLanding)
 
 fun main() {
-    val listOfPlanets = mutableListOf<Planet>()
-    val listOfSatellite = mutableListOf<Satellite>()
-
     val planetMars = Planet("Марс", false, true)
-    listOfPlanets.add(planetMars)
-
-    val satellitePhobos = Satellite("Фобос", false, false, "Марс")
-    val satelliteDeimos = Satellite("Деймос", false, true, "Марс")
-    listOfSatellite.add(satelliteDeimos)
-    listOfSatellite.add(satellitePhobos)
-
-    for (satellite in listOfSatellite) {
-        for (planet in listOfPlanets) {
-            if (satellite.belongsToPlanet == planet.name) {
-                planet.addSatellite(satellite)
-            }
-        }
-    }
-
+    val listOfMarsSatellite = listOf(
+        Satellite("Фобос", false, false),
+        Satellite("Деймос", false, true)
+    )
+    planetMars.addSatellite(listOfMarsSatellite)
     planetMars.printPlanetAndSatelliteInfo()
 }
