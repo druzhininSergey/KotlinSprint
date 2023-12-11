15f2eeabc0ff4f5f8a15e334b405bee0005d8fcb
sakhb <druzhininsergey96@gmail.com>
2023-12-11 20:19:36 +1100

    KS-14-1 deleted Ship class

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,31 +1,26 @@
 package lesson_14
 
-open class Ship(
-    val shipName: String,
-    val speed: Int,
-    val loadCapacity: Int,
-    val hasIceBreakingAbility: Boolean = false,
+open class Liner(
+    val shipName: String = "Лайнер",
+    val speed: Int = 80,
+    val loadCapacity: Int = 50,
+    val passengers: Int = 500,
 )
 
 class CargoShip(
     shipName: String = "Грузовой корабль",
     speed: Int = 50,
     loadCapacity: Int = 500,
-) : Ship(shipName, speed, loadCapacity)
+    passengers: Int = 80,
+) : Liner (shipName, speed, loadCapacity, passengers)
 
 class IceBreaker(
     shipName: String = "Ледокол",
     speed: Int = 40,
     loadCapacity: Int = 150,
-    hasIceBreakingAbility: Boolean = true,
-) : Ship(shipName, speed, loadCapacity, hasIceBreakingAbility)
-
-class Liner(
-    shipName: String = "Лайнер",
-    speed: Int = 80,
-    loadCapacity: Int = 200,
-    val passengers: Int = 500,
-) : Ship(shipName, speed, loadCapacity)
+    passengers: Int = 150,
+    val hasIceBreakingAbility: Boolean = true,
+) : Liner(shipName, speed, loadCapacity, passengers)
 
 fun main() {
     val cargoShip = CargoShip()
