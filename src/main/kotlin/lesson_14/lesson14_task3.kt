bea63f305145e927be614737305671755b9bf898
sakhb <druzhininsergey96@gmail.com>
2023-12-11 22:20:10 +1100

    KS-14-3 add fun to Figure class

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -8,6 +8,19 @@ abstract class Figure(
     abstract fun calculateArea(): Double
     abstract fun calculatePerimeter(): Double
 
+    companion object {
+        fun calculateSumByColor(list: MutableList<Figure>, color: String): Pair<Double, Double> {
+            val areaList = mutableListOf<Double>()
+            val perimeterList = mutableListOf<Double>()
+            for (i in list) {
+                if (i.color == color) {
+                    areaList.add(i.calculateArea())
+                    perimeterList.add(i.calculatePerimeter())
+                }
+            }
+            return areaList.sum() to perimeterList.sum()
+        }
+    }
 }
 
 class Circle(
@@ -31,6 +44,7 @@ class Rectangle(
     private val width: Double,
     private val length: Double,
 ) : Figure(color) {
+
     override fun calculateArea(): Double {
         val area = width * length
         return area
@@ -49,22 +63,12 @@ fun main() {
     val rectangle2 = Rectangle("Черный", 30.2, 28.8)
 
     val figuresList = mutableListOf(circle1, circle2, rectangle1, rectangle2)
-    val whiteAreaList = mutableListOf<Double>()
-    val whitePerimeterList = mutableListOf<Double>()
-    val blackAreaList = mutableListOf<Double>()
-    val blackPerimeterList = mutableListOf<Double>()
 
-    for (i in figuresList) {
-        if (i.color == "Белый") {
-            whiteAreaList.add(i.calculateArea())
-            whitePerimeterList.add(i.calculatePerimeter())
-        } else if (i.color == "Черный") {
-            blackAreaList.add(i.calculateArea())
-            blackPerimeterList.add(i.calculatePerimeter())
-        }
-    }
-    println("Сумма площадей белых фигур = ${"%.1f".format(whiteAreaList.sum())}")
-    println("Сумма периметров белых фигур = ${"%.1f".format(whitePerimeterList.sum())}")
-    println("Сумма площадей черных фигур = ${"%.1f".format(blackAreaList.sum())}")
-    println("Сумма периметров черных фигур = ${"%.1f".format(blackPerimeterList.sum())}")
+    val (whiteAreaList, whitePerimeterList) = Figure.calculateSumByColor(figuresList, "Белый")
+    val (blackAreaList, blackPerimeterList) = Figure.calculateSumByColor(figuresList, "Черный")
+
+    println("Сумма площадей белых фигур = ${"%.1f".format(whiteAreaList)}")
+    println("Сумма периметров белых фигур = ${"%.1f".format(whitePerimeterList)}")
+    println("Сумма площадей черных фигур = ${"%.1f".format(blackAreaList)}")
+    println("Сумма периметров черных фигур = ${"%.1f".format(blackPerimeterList)}")
 }
