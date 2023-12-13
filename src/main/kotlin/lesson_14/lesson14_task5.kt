637cee4f7ae2688cb1642e7f8eefbc16a150d36e
sakhb <druzhininsergey96@gmail.com>
2023-12-14 01:00:53 +1100

    KS-14-5 deleted companion object

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -14,35 +14,34 @@ class ChildMessage(
 ) : Message(id, userName, userMessage)
 
 class Chat() {
+    val listOfMessages = mutableListOf<Message>()
+    var id = 0
+    fun addMessage(userName: String, text: String) {
+        val userMessage = Message(id = ++id, userName = userName, userMessage = text)
+        listOfMessages.add(userMessage)
+    }
 
-    companion object {
-        val listOfMessages = mutableListOf<Message>()
-        var id = 0
-        fun addMessage(userName: String, text: String) {
-            val userMessage = Message(id = ++id, userName = userName, userMessage = text)
-            listOfMessages.add(userMessage)
-        }
+    fun addThreadMessage(userName: String, text: String, parentMessageId: Int) {
+        val userChildMessage =
+            ChildMessage(id = ++id, userName = userName, userMessage = text, parentMessageId = parentMessageId)
+        listOfMessages.add(userChildMessage)
+    }
 
-        fun addThreadMessage(userName: String, text: String, parentMessageId: Int) {
-            val userChildMessage =
-                ChildMessage(id = ++id, userName = userName, userMessage = text, parentMessageId = parentMessageId)
-            listOfMessages.add(userChildMessage)
-        }
-
-        fun printChat(level: Int = 0) {
-            val groupedMessages = listOfMessages.groupBy({ if (it is ChildMessage) it.parentMessageId else it.id }, { it.userMessage })
-            println(groupedMessages)
-        }
+    fun printChat(level: Int = 0) {
+        val groupedMessages =
+            listOfMessages.groupBy({ if (it is ChildMessage) it.parentMessageId else it.id }, { it.userMessage })
+        println(groupedMessages)
     }
 }
 
 fun main() {
-    val message1 = Chat.addMessage("Сергей", "Всем привет")
-    val message2 = Chat.addMessage("Петя", "Какая завтра погода?")
-    val message3 = Chat.addMessage("Катя", "Здравствуйте")
-    val message4 = Chat.addThreadMessage("Женя", "29 градусов", 2)
-    val message5 = Chat.addThreadMessage("Саша", "Привет Катя", 3)
-    val message6 = Chat.addThreadMessage("Клава", "Вроде будет 25 градусов и дождь", 2)
+    val message = Chat()
+    message.addMessage("Сергей", "Всем привет")
+    message.addMessage("Петя", "Какая завтра погода?")
+    message.addMessage("Катя", "Здравствуйте")
+    message.addThreadMessage("Женя", "29 градусов", 2)
+    message.addThreadMessage("Саша", "Привет Катя", 3)
+    message.addThreadMessage("Клава", "Вроде будет 25 градусов и дождь", 2)
 
-    Chat.printChat()
+    message.printChat()
 }
