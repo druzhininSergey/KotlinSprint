4cd1b5e6d3bb648e24e50c004cd804fcf0b649cf
sakhb <druzhininsergey96@gmail.com>
2023-12-29 21:50:27 +1100

    KS-19-1 rename function

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -3,31 +3,31 @@ package lesson_19
 fun main() {
     println("Вы можете добавить в аквариум следующих рыбок:")
     for (i in Fish.entries){   // Среда разработки предлагает использование entries взамен values()
-        println(i.printFishName())
+        println(i.getFishName())
     }
 }
 
 enum class Fish{
     GUPPY() {
-        override fun printFishName(): String {
+        override fun getFishName(): String {
             return "Гуппи"
         }
     },
     ANGEL_FISH() {
-        override fun printFishName(): String {
+        override fun getFishName(): String {
            return "Скалярия"
         }
     },
     GOLDEN_FISH() {
-        override fun printFishName(): String {
+        override fun getFishName(): String {
             return "Золотая рыбка"
         }
     },
     SIAMESE_FIGHTING_FISH() {
-        override fun printFishName(): String {
+        override fun getFishName(): String {
             return "Петушок"
         }
     };
 
-    abstract fun printFishName() : String
+    abstract fun getFishName() : String
 }
