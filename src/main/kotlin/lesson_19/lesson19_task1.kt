6358f186eaffb9940a35457c0bf6c452b848b59b
sakhb <druzhininsergey96@gmail.com>
2024-01-23 02:39:05 +1100

    KS-19-1 deleted abstract fun

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -2,32 +2,12 @@ package lesson_19
 
 fun main() {
     println("Вы можете добавить в аквариум следующих рыбок:")
-    for (i in Fish.entries){   // Среда разработки предлагает использование entries взамен values()
-        println(i.getFishName())
-    }
+    for (i in Fish.entries) println(i.fishName)
 }
 
-enum class Fish{
-    GUPPY() {
-        override fun getFishName(): String {
-            return "Гуппи"
-        }
-    },
-    ANGEL_FISH() {
-        override fun getFishName(): String {
-           return "Скалярия"
-        }
-    },
-    GOLDEN_FISH() {
-        override fun getFishName(): String {
-            return "Золотая рыбка"
-        }
-    },
-    SIAMESE_FIGHTING_FISH() {
-        override fun getFishName(): String {
-            return "Петушок"
-        }
-    };
-
-    abstract fun getFishName() : String
+enum class Fish(val fishName: String) {
+    GUPPY("Гуппи"),
+    ANGEL_FISH("Скалярия"),
+    GOLDEN_FISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
