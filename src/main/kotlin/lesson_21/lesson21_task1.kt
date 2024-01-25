4bad7e180e1b9f273af13cdf595be1da96cf86b9
sakhb <druzhininsergey96@gmail.com>
2024-01-26 07:37:07 +1100

    KS-21-1 count vowels

---
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -0,0 +1,14 @@
+package lesson_21
+
+fun String.countVowels(): Int {
+    val vowels = setOf("a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y")
+    val symbols = this.split("")
+    val count = symbols.count { it in vowels }
+    println("В строке \"$this\" находятся $count гласных букв")
+    return count
+}
+
+fun main() {
+    val string = "hello, how are you?"
+    string.countVowels()
+}
