6168012ef094b0b741b745586bac7529d7b37184
sakhb <druzhininsergey96@gmail.com>
2024-02-01 13:03:36 +1100

    KS-11-5 deleted companion object

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -11,33 +11,31 @@ class ForumMessage(
 )
 
 class Forum {
+    private var id = 0
+    private val allUsers = mutableListOf<ForumMember>()
+    private val allMessages = mutableListOf<ForumMessage>()
 
-    companion object {
-        private var id = 0
-        private val allUsers = mutableListOf<ForumMember>()
-        private val allMessages = mutableListOf<ForumMessage>()
+    fun createNewUser(userName: String): ForumMember {
+        val member = ForumMember(userId = ++id, userName = userName)
+        allUsers.add(member)
+        println("Создан аккаунт пользователя.\nЛогин: ${member.userName}\nid: ${member.userId}")
+        return member
+    }
 
-        fun createNewUser(userName: String): ForumMember {
-            val member = ForumMember(userId = ++id, userName = userName)
-            allUsers.add(member)
-            return member
-        }
+    fun createNewMessage(id: Int, message: String): ForumMessage? {
+        val checkId = allUsers.filter { it.userId == id }
+        if (checkId.isNotEmpty()) {
+            val newMessage = ForumMessage(id, message)
+            allMessages.add(newMessage)
+            return newMessage
+        } else return null
+    }
 
-        fun createNewMessage(id: Int, message: String): ForumMessage? {
-            val checkId = allUsers.filter { it.userId == id }
-            if (checkId.isNotEmpty()) {
-                val newMessage = ForumMessage(id, message)
-                allMessages.add(newMessage)
-                return newMessage
-            } else return null
-        }
-
-        fun printThread() {
-            for (i in allMessages){
-                for (user in allUsers){
-                    if (user.userId == i.authorId)
+    fun printThread() {
+        for (i in allMessages) {
+            for (user in allUsers) {
+                if (user.userId == i.authorId)
                     println("${user.userName}: ${i.message}")
-                }
             }
         }
     }
@@ -45,11 +43,14 @@ class Forum {
 }
 
 fun main() {
+    val forum = Forum()
+    val user1 = forum.createNewUser("Пользователь 1")
+    val user2 = forum.createNewUser("Пользователь 2")
 
-    val user1 = Forum.createNewUser(readln())
-    val user2 = Forum.createNewUser(readln())
-    val message1 = Forum.createNewMessage(readln().toIntOrNull() ?: 0, readln())
-    val message2 = Forum.createNewMessage(readln().toIntOrNull() ?: 0, readln())
+    println("Впишите id ${user1.userName} для отправки сообщения")
+    val message1 = forum.createNewMessage(readln().toIntOrNull() ?: 0, "Привет")
+    println("Впишите id ${user2.userName} для отправки сообщения")
+    val message2 = forum.createNewMessage(readln().toIntOrNull() ?: 0, "Пока")
 
-    Forum.printThread()
+    forum.printThread()
 }
